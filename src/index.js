8c50e433cf21ebc61a84f7a455c9a5c1e724e171
William Kay Kephart <wllmkphrt@gmail.com>
2022-11-21 00:52:38 -0500

    Adjusted appearance, made deck background opaque

--- src/index.js
+++ src/index.js
@@ -13,13 +13,15 @@ function appendArt(data){
     data.forEach((element) => {
         let img = document.createElement("img");
         img.src = element.art;
-        img.width = "225";
-        img.height = "225";
+        img.width = "200";
+        img.height = "200";
         mainContainer.appendChild(img);
         img.addEventListener("click", function addCardtoDeck(){
             deck1.deckList[deck1.cardCount] = element;
             document.getElementById(`card`+`${(deck1.cardCount + 1)}`).src = element.art;
-            deck1.cardCount++;
+            if(deck1.cardCount < 12){
+                deck1.cardCount++;
+            }
         })
     })
 }
