8b437e02b4c94150805b2cf6e87df03d79d14442
William Kay Kephart <wllmkphrt@gmail.com>
2022-11-22 21:18:28 -0500

    Changed styling, added text under cards.

--- src/index.js
+++ src/index.js
@@ -9,13 +9,23 @@ const deck1 = new Deck();
 
 
 function appendArt(data){
-    var mainContainer = document.getElementById("myData");
+    let mainContainer = document.getElementById("myData");
     data.forEach((element) => {
+        let card = document.createElement("div");
+        card.id = element.id;
+        card.className = "card";
+        mainContainer.appendChild(card);
         let img = document.createElement("img");
         img.src = element.art;
-        img.width = "210";
-        img.height = "210";
-        mainContainer.appendChild(img);
+        img.width = "190";
+        img.height = "190";
+        card.appendChild(img);
+        let p = document.createElement("p");
+        p.id = "text";
+        let effect = document.createTextNode(`${element.effect}`);
+        p.appendChild(effect);
+        card.appendChild(p);
+
         img.addEventListener("click", function addCard(){
             if(deck1.art.filter(currentValue => !currentValue).length < 12){
                 const indexOfDefaultArt = deck1.art.findIndex(currentValue => currentValue);
@@ -23,13 +33,13 @@ function appendArt(data){
                 deck1.art[indexOfDefaultArt] = false;
                 const currentImg = document.getElementById(`card`+`${indexOfDefaultArt + 1}`);
                 currentImg.src = element.art;
-                img.style.display ='none';
+                card.style.display ='none';
                 currentImg.addEventListener("click", function removeCard(){
                     const index = deck1.deckList.indexOf(element);
                     if (index > -1){
                         deck1.art[index] = true;
                         currentImg.src ="src/images/snap-logo.webp";
-                        img.style.display ='inline';
+                        card.style.display ='inline-block';
                     }
                 },{once: true})
             }
